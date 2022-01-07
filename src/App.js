30570de019ad8ed9d9509a61ecaa1a6bbe65b8ea
Diego <drbv27@gmail.com>
2022-01-07 08:59:54 -0500

    Validating inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import { useFormik } from "formik";
 
+const validate = (values) => {
+  const errors = {};
+  if (!values.name) {
+    errors.name = "Requerido";
+  } else if (values.name.length < 5) {
+    errors.name = "Requiere al menos 6 caracteres";
+  }
+  if (!values.lastname) {
+    errors.lastname = "Requerido";
+  } else if (values.lastname.length < 5) {
+    errors.lastname = "Requiere al menos 6 caracteres";
+  }
+  return errors;
+};
+
 function App() {
   const formik = useFormik({
     initialValues: {
@@ -7,6 +22,7 @@ function App() {
       lastname: "",
       email: "",
     },
+    validate,
     onSubmit: (values) => console.log(values),
   });
   return (
@@ -18,6 +34,8 @@ function App() {
         onChange={formik.handleChange}
         value={formik.values.name}
       />
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      <br />
       <label>Apellido: </label>
       <input
         name="lastname"
@@ -25,6 +43,8 @@ function App() {
         onChange={formik.handleChange}
         value={formik.values.lastname}
       />
+      {formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
+      <br />
       <label>Email: </label>
       <input
         name="email"
